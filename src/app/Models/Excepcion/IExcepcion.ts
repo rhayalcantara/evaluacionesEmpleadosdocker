457d85dcb8ab64781065bb97706dfd90d325d078
rhayalcantara <rhayalcantara@gmail.com>
2024-10-10 09:23:37 -0400

    modelo IException creado

---
+++ src/app/Models/Excepcion/IExcepcion.ts
@@ -0,0 +1,13 @@
+import { IEmpleado } from "../Empleado/IEmpleado";
+
+export interface IException {
+    id: number;
+    tipo: string;
+    empleadoSecuencial: number;
+    detalles: string;
+    fecha: string;
+    activa: boolean;
+  }
+export interface IExceptionDts extends IException{
+    empleado:IEmpleado
+}
