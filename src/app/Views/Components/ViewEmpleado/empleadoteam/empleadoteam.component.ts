e3bac9711d265fe2e98a855df0ea689bbe8f2482
rhayalcantara <rhayalcantara@gmail.com>
2024-10-16 09:19:23 -0400

    se agrego un filtro al equipo

--- src/app/Views/Components/ViewEmpleado/empleadoteam/empleadoteam.component.ts
+++ src/app/Views/Components/ViewEmpleado/empleadoteam/empleadoteam.component.ts
@@ -11,47 +11,58 @@ import { IPeriodo } from 'src/app/Models/Periodos/IPeriodo';
 import { CardEmpleadoComponent2 } from '../card-empleado2/card-empleado.component';
 import { ObjetivosComponent } from '../../Pages/objetivos/objetivos.component';
 
-
 @Component({
   selector: 'app-empleadoteam',
-  standalone:true,
-  imports:[FormsModule, CommonModule, 
-    CardEmpleadoComponent,CardEmpleadoComponent2],
+  standalone: true,
+  imports: [FormsModule, CommonModule, 
+    CardEmpleadoComponent, CardEmpleadoComponent2],
   templateUrl: './empleadoteam.component.html',
   styleUrls: ['./empleadoteam.component.css']
 })
 export class EmpleadoTeamComponent implements OnInit {
   
-  @Input() empleado: IEmpleado=this.empl.inicializamodelo();
-  @Input() periodo: IPeriodo =this.peri.inicializamodelo();
+  @Input() empleado: IEmpleado = this.empl.inicializamodelo();
+  @Input() periodo: IPeriodo = this.peri.inicializamodelo();
 
-  constructor(public empl: Empleados,
+  searchTerm: string = '';
+  filteredSubordinados: IEmpleado[] = [];
+
+  constructor(
+    public empl: Empleados,
     private dialog: MatDialog,
-    private peri:Periodos,
-    
-    ){}
+    private peri: Periodos,
+  ) {}
+
   ngOnInit(): void {
-  
-   
+    this.filteredSubordinados = this.empl.arraymodelsubordinados;
+  }
+
+  searchSubordinados(): void {
+    if (!this.searchTerm.trim()) {
+      this.filteredSubordinados = this.empl.arraymodelsubordinados;
+    } else {
+      this.filteredSubordinados = this.empl.arraymodelsubordinados.filter(subordinado =>
+        subordinado.nombreunido.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        subordinado.departamento.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        subordinado.cargo.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 
   agregametasub() {
-    // Implement the agregametasub functionality here
     console.log('Agregar metas para subordinados');
-    // You may need to inject necessary services and implement the logic
     const dialogRef = this.dialog.open(SupervisorGoalsComponent, {
-      width: '1200px', data: { empl: this.empl,periodo:this.periodo }
+      width: '1200px', data: { empl: this.empl, periodo: this.periodo }
     });
     dialogRef.afterClosed().subscribe((rep) => {
       
     });
   }
+
   objetivoscall() {
-    // Implement the agregametasub functionality here
     console.log('Agregar metas para subordinados');
-    // You may need to inject necessary services and implement the logic
     const dialogRef = this.dialog.open(ObjetivosComponent, {
-      width: '100%',height:'80%', data: { empl: this.empl,periodo:this.periodo }
+      width: '100%', height: '80%', data: { empl: this.empl, periodo: this.periodo }
     });
     dialogRef.afterClosed().subscribe((rep) => {
       
