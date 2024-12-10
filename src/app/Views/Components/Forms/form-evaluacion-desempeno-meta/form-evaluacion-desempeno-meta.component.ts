1ec460e7a18bf11a075890f7cfa519e8fcdbf5a4
rhayalcantara <rhayalcantara@gmail.com>
2024-12-10 10:13:26 -0400

    se agrego el empleado y peso al formulario de evaluacion-desempeño-meta

--- src/app/Views/Components/Forms/form-evaluacion-desempeno-meta/form-evaluacion-desempeno-meta.component.ts
+++ src/app/Views/Components/Forms/form-evaluacion-desempeno-meta/form-evaluacion-desempeno-meta.component.ts
@@ -1,36 +1,64 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { IEvaluacionDesempenoMeta } from 'src/app/Models/EvaluacionDesempenoMeta/IEvaluacionDesempenoMeta';
 import { DatosServiceService } from 'src/app/Services/datos-service.service';
 import { CommonModule } from '@angular/common';
 import { EvaluacionDesempenoMeta } from 'src/app/Controllers/EvaluacionDesempenoMeta';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { SeleccionEmpleadoComponent } from '../seleccion-empleado/seleccion-empleado.component';
+import { Empleados } from 'src/app/Controllers/Empleados';
+import { IEmpleado } from 'src/app/Models/Empleado/IEmpleado';
 
 @Component({
   selector: 'app-form-evaluacion-desempeno-meta',
   templateUrl: './form-evaluacion-desempeno-meta.component.html',
   styleUrls: ['./form-evaluacion-desempeno-meta.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, MatDialogModule, CommonModule]
+  imports: [ReactiveFormsModule, MatDialogModule, CommonModule, MatButtonModule, MatSelectModule]
 })
 export class FormEvaluacionDesempenoMetaComponent implements OnInit {
   fg: FormGroup;
   titulo: string = 'Nueva Meta de Evaluación';
+  tipoOptions: string[] = ['KRI', 'KPI', 'OBJETIVO', 'PROYECTO'];
+  selectedEmpleado: IEmpleado | undefined = {
+    secuencial: 0,
+    codigousuario: '',
+    nombreunido: 'Seleccione un empleado',
+    identificacion: '',
+    sdept: 0,
+    departamento: '',
+    codigoestado: '',
+    scargo: 0,
+    cargo: '',
+    esjefatura: 0,
+    tienejefe: 0,
+    nivel: 0,
+    fechapostulacion: '',
+    jefeinmediatO_SECUENCIAL: 0,
+    jefeinmediato: ''
+  };
 
   constructor(
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<FormEvaluacionDesempenoMetaComponent>,
+    private dialogmat: MatDialog,
     public metaService: EvaluacionDesempenoMeta,
+    public empleadocontroller: Empleados,
     @Inject(MAT_DIALOG_DATA) public data: { model: IEvaluacionDesempenoMeta },
-    private datosService: DatosServiceService
+    private datosService: DatosServiceService,
+    private cd: ChangeDetectorRef
   ) {
     this.fg = this.fb.group({
       Id: [0],
       EvaluacionId: [0],
-      Tipo: ['', Validators.required],
+      tipo: ['', Validators.required],
       descripcion: ['', Validators.required],
       meta: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
-      inverso: [false]
+      peso: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+      inverso: [false],
+      secuencialId: [0, Validators.required] // Using secuencialId for employee reference
     });
   }
 
@@ -39,11 +67,37 @@ export class FormEvaluacionDesempenoMetaComponent implements OnInit {
       this.titulo = 'Editar Meta de Evaluación';            
     }
     this.fg.patchValue(this.data.model);
-    console.log(this.fg.value);
+    this.empleadocontroller.getdatos();
+
+    this.empleadocontroller.TRegistros.subscribe(() => {
+      if (this.fg.get('secuencialId')?.value) {
+        this.selectedEmpleado = this.getempleado(this.fg.get('secuencialId')?.value);
+      }      
+      this.cd.detectChanges();
+    });
+  }
+
+  getempleado(id: number): IEmpleado | undefined {
+    return this.empleadocontroller.arraymodel.find((empleado) => empleado.secuencial === id);
+  }
+
+  buscarEmpleado(): void {
+    const dialogRef = this.dialogmat.open(SeleccionEmpleadoComponent, {
+      width: '800px',
+      data: {}
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.fg.patchValue({ secuencialId: result.secuencial });
+        this.selectedEmpleado = result;
+        console.table(result);
+        console.table(this.fg.value);
+      }
+    });
   }
 
   grabar(): void {
-    console.table( this.fg.value);
+    console.table(this.fg.value);
     if (this.fg.valid) {
       const meta: IEvaluacionDesempenoMeta = this.fg.value;      
       
